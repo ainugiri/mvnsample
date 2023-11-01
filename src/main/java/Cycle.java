5834e6651737f5f78f4b9dcd420366b0831c3845
ainugiri <40005294+ainugiri@users.noreply.github.com>
2023-11-01 12:09:48 +0530

    Create Cycle.java

---
+++ src/main/java/Cycle.java
@@ -0,0 +1,9 @@
+public class Cycle {
+    int model;
+    String manuName;
+    public void display(){
+        System.out.println("Cycle model" +model);
+        System.out.println("Cycle manuName" +manuName);
+
+    }
+}
